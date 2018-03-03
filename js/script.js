35e6d9517bd43632feef0e09d7bec2c902be052e
geekayush <srivastavs61@gmail.com>
2018-03-03 12:08:14 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+    document.getElementById('navbar').classList.toggle('enlarge');
+    document.getElementById('sher').classList.toggle('hidden');
+    document.getElementById('answer').classList.toggle('hidden');
+    document.getElementById('solve').classList.toggle('hidden');
+}
