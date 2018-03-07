0b65b2446c2630295a7b74760503ddccbd89ba20
geekayush <srivastavs61@gmail.com>
2018-03-07 16:37:58 +0530

    Update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
+function one(element, eventType, callback, self) {
+    var one = function (event) {
+        try {
+            callback.call(self, event);
+        } finally {
+            element.removeEventListener(eventType, one);
+        }
+    }
+    element.addEventListener(eventType, one);
+}
+
 function toggleNav() {
     document.getElementById('navbar').classList.toggle('enlarge');
     document.getElementById('sher').classList.toggle('hidden');
     document.getElementById('answer').classList.toggle('hidden');
     document.getElementById('solve').classList.toggle('hidden');
+}
+
+document.getElementById('description_container').one('click', function (e) {
+    e.classList.remove('hidden');
+})
+
+function open_des(e) {
+    var x = document.getElementById('description_container');
+    x.classList.remove('hidden');
+    x.classList.add('fadeInDownBig');
+    x.classList.remove('fadeOutDownBig');
+}
+
+function close_des() {
+    var y = document.getElementById('description_container');
+    y.classList.add('fadeOutDownBig');
+    y.classList.remove('fadeInDownBig');
 }
